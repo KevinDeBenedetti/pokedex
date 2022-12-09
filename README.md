0d545e1db5e1276472609fcdb369d34ef78dd117
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2022-12-09 23:08:58 +0100

    modal
    Ajout d'une fenêtre modale
    HTML & CSS en cours

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # pokedex
+
+## Colors 
+
+### Grayscale 
+- Dark Gray #212121
+- Medium Gray #666666
+- Light Gray #E0E0E0
+- White #FFFFFF
+- Background #F7F7F7
+
+### Pokemon Type
+- Rock #B69E31 
+- Ghost #70559B
+- Steel #B7B9D0
+- Water #6493EB
+- Grass #74CB48
+- Psychic #FB5584
+- Ice #9AD6DF
+- Dark #75574C
+- Fairy #E69EAC
+- Normal #AAA67F
+- Fighting #C12239
+- Flying #A891EC
+- Poison #A43E9E
+- Ground #DEC16B
+- Bug #A7B723
+- Fire #F57D31
+- Electric #F9CF30
+- Dragon #7037FF
+
+## Fonts
+Poppings : Regular / Bold
+https://fonts.google.com/specimen/Poppins
+
+### Icons 
+https://fonts.google.com/icons
--- script.js
+++ script.js
@@ -4,9 +4,7 @@ let pokeList = document.querySelector('#pokemon-list');
 function fetchPokemon(id) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
         .then(response => response.json())
-        .then(data => {
-            createPokemon(data);
-        });
+        .then(data => {createPokemon(data)});
 }
 // Fonction pour itérer les pokemon souhaité
 function fetchPokemons(number) {
@@ -38,20 +36,54 @@ function createPokemon(pokemon) {
     card.appendChild(name);
 
     pokeList.appendChild(card);
-}
-// Fonction pour afficher une fenêtre modale lorsque l'on clic sur la card
-function createDetails(pokemon) {
+
+    // Test pour data-
 
 }
-// Fenêtres modales
-//
-// Les variables à utiliser dans
-let body = document.querySelector("body");
-let openModalBtn = document.querySelector(".open-modal-btn");
-let mainWrapper = document.querySelector(".main-wrapper");
-let modal = document.querySelector(".modal");
-let modalTitle = document.querySelector(".modal-title")
-// Événements pour les fenêtres modales
+
+// Modale
+window.onload = () => {
+    // On récupère tous les boutons d'ouverture de modale
+    const modalButtons = document.querySelectorAll("[data-toggle=modal]");
+    
+    for(let button of modalButtons){
+        button.addEventListener("click", function(e){
+            // On empêche la navigation
+            e.preventDefault();
+            // On récupère le data-target
+            let target = this.dataset.target
+            
+            // On récupère la bonne modale
+            let modal = document.querySelector(target);
+            // On affiche la modale
+            modal.classList.add("show");
+
+            // On récupère les boutons de fermeture
+            const modalClose = modal.querySelectorAll("[data-dismiss=dialog]");
+            
+            for(let close of modalClose){
+                close.addEventListener("click", () => {
+                    modal.classList.remove("show");
+                });
+            }
+            // On récupère le num du Pokemon appelé
+            let detailsId = this.dataset.id;
+            // On affiche la bonne destination dans la modale
+            let modalId = document.querySelector(".modal-id")
+            // On affiche la bonne valeur
+            
+            // On gère la fermeture lors du clic sur la zone grise
+            modal.addEventListener("click", function(){
+                this.classList.remove("show");
+            });
+            // On évite la propagation du clic d'un enfant à son parent
+            modal.children[0].addEventListener("click", function(e){
+                e.stopPropagation();
+            })
+        });
+    }
+
+}
 
 // Nombre de pokemon à afficher à l'utilisateur
 fetchPokemons(151);
